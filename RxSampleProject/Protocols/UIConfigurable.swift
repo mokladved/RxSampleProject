818e251f3995e97e57ddc120e073d291e21a3ef7
moklad <dev.moklad@gmail.com>
2025-08-20 03:09:49 +0900

    feat: 일관되고 구조적인 UI 구현을 위한 UIConfiugrable 프로토콜 생성

---
+++ RxSampleProject/Protocols/UIConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  UIConfigurable.swift
+//  RxSampleProject
+//
+//  Created by Youngjun Kim on 8/20/25.
+//
+
+import Foundation
+
+protocol UIConfigurable {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
