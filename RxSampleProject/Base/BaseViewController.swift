af1a8bb6ce9308cfc00457bd280c9fc2479837b2
moklad <dev.moklad@gmail.com>
2025-08-20 03:43:49 +0900

    fix: 초기화 메서드 누락된 부분 추가

--- RxSampleProject/Base/BaseViewController.swift
+++ RxSampleProject/Base/BaseViewController.swift
@@ -14,6 +14,9 @@ class BaseViewController: UIViewController, UIConfigurable {
         configureLayout()
         configureView()
     }
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+            super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
