af9ccfc8ea3ed4791f7a331e9cba57ba31a5cb40
moklad <dev.moklad@gmail.com>
2025-08-20 04:06:57 +0900

    feat: rx를 이용하여 테이블 뷰 구현

--- RxSampleProject/Views/SimpleTableViewExampleViewController.swift
+++ RxSampleProject/Views/SimpleTableViewExampleViewController.swift
@@ -6,12 +6,67 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class SimpleTableViewExampleViewController: UIViewController {
-
+final class SimpleTableViewExampleViewController: BaseViewController {
+    private let tableView = {
+        let tableView = UITableView()
+        tableView.register(SimpleTableViewCell.self, forCellReuseIdentifier: SimpleTableViewCell.identifier)
+        tableView.rowHeight = 52
+        return tableView
+    }()
+    
+    private let disposeBag = DisposeBag()
+    
+    let items = Observable.just(
+        (0..<20).map { "\($0)" }
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        showData()
+    }
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        view.addSubview(tableView)
+    }
+    
+    override func configureView() {
+        super.configureView()
+        self.title = "Simple table view"
+        tableView.separatorStyle = .singleLine
+    }
+    
+    
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func showData() {
+        items.bind(to: tableView.rx.items(cellIdentifier: "SimpleTableViewCell", cellType: SimpleTableViewCell.self)) {
+            (row, element, cell) in
+            cell.infoLabel.text = "\(element) @ row \(row)"
+            cell.accessoryType = .detailButton
+
+        }.disposed(by: disposeBag)
+        
+        tableView.rx.modelSelected(String.self)
+            .bind(with: self) { owner, value in
+                owner.showAlert(title: "ReExample", message: "Tapped `\(value)`")
+            }
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .itemAccessoryButtonTapped
+            .bind(with: self, onNext: { owner, indexPath in
+                owner.showAlert(title: "ReExample", message: "Tapped Detail @ \(indexPath.section),\(indexPath.row)")
+            })
+            .disposed(by: disposeBag)
     }
 }
 
